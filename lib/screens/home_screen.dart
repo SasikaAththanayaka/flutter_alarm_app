81c7d417e965fce64997f9e723f0ca670bbbb9c4
SasikaAththanaya <sasikaharshana97@gmail.com>
2022-01-09 21:12:28 +0530

    clock was created

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,20 @@
+import 'package:alarm_app/widgets/clock_view.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        alignment: Alignment.center,
+        color: Color(0xFF2D2F41),
+        child: ClockView(),
+      ),
+    );
+  }
+}
