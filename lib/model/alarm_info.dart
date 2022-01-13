c2baed9c3953d68af82307a457e9d47dfed4c215
SasikaAththanaya <sasikaharshana97@gmail.com>
2022-01-13 20:57:35 +0530

    alarm info model was created

---
+++ lib/model/alarm_info.dart
@@ -0,0 +1,11 @@
+class AlarmInfo {
+  DateTime alarmDateTime;
+  String description;
+  bool isActive;
+
+  AlarmInfo(this.alarmDateTime, {this.description});
+}
+
+List<AlarmInfo> alarm = [
+  AlarmInfo(DateTime.now().add(Duration(hours: 1)), description: "Office"),
+];
